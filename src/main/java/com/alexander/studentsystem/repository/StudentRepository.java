ef0bea2d8050252af0596ae1feac98219b726cf1
alexoviflo <alexander.ovesen@online.no>
2024-08-22 12:03:38 +0200

    Initial commit

---
+++ src/main/java/com/alexander/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.alexander.studentsystem.repository;
+
+import com.alexander.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
